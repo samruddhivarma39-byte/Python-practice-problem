2185d4023e602cd7fedb86f27e393e0979bfaefb
samruddhivarma39-byte <samruddhivarma39@gmail.com>
2026-08-12 16:10:14 +0530

    Add files via upload

---
+++ pos+,neg-,zero(0).py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number:")) 
+if num>0:  
+    print("Given Number is Positive") 
+elif num<0:  
+    print("Given Number is Negative")
+else:  
+    print("Given Number is zero")
