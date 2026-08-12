54fe9d426a439029f3eb9e8446179224638b57ac
samruddhivarma39-byte <samruddhivarma39@gmail.com>
2026-08-12 16:12:23 +0530

    Add files via upload

---
+++ largest numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first: ")) 
+b = int(input("Enter second: ")) 
+c = int(input("Enter third: ")) 
+if a >c and a>b:  
+    print(a,"is largest") 
+elif b>c and b>a:  
+    print(b,"is largest") 
+else:  
+    print(c,"is largest")
